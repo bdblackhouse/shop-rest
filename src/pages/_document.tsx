156f0f1f63861ee1ac8cfbc114d0ef2296004cc6
Minhazul-Islam18 <minhaz019166@gmail.com>
2025-01-21 20:14:13 +0600

    Tawk.to updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,7 @@ export default class CustomDocument extends Document {
                 (function(){
                   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
                   s1.async=true;
-                  s1.src='https://embed.tawk.to/621e34761ffac05b1d7c6ba4/1ft2vpsnj';
+                  s1.src='https://embed.tawk.to/678f99c03a8427326072467f/1ii4f16kl';
                   s1.charset='UTF-8';
                   s1.setAttribute('crossorigin','*');
                   s0.parentNode.insertBefore(s1,s0);
