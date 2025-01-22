8b6af4e15394a25d74a543742b02a07e0bd9a37d
Minhazul-Islam18 <minhaz019166@gmail.com>
2025-01-22 20:37:13 +0600

    Homepage updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,65 @@
 import BannerCard from '@components/common/banner-card';
 import Container from '@components/ui/container';
-import BrandGridBlock from '@containers/brand-grid-block';
-import CategoryBlock from '@containers/category-block';
-import { getLayout } from '@components/layout/layout';
-import BannerWithProducts from '@containers/banner-with-products';
-import BannerBlock from '@containers/banner-block';
+import CollectionBlock from '@containers/collection-block';
+import BannerCarouselBlock from '@containers/banner-carousel-block';
 import Divider from '@components/ui/divider';
 import DownloadApps from '@components/common/download-apps';
 import Support from '@components/common/support';
-import Instagram from '@components/common/instagram';
-import ProductsFlashSaleBlock from '@containers/product-flash-sale-block';
-import ProductsFeatured from '@containers/products-featured';
-import BannerSliderBlock from '@containers/banner-slider-block';
-import ExclusiveBlock from '@containers/exclusive-block';
 import Subscription from '@components/common/subscription';
+import HeroBlock from '@containers/hero-block';
+import BrandBlock from '@containers/brand-block';
+import CategoryBlock from '@containers/category-block';
+import FeatureBlock from '@containers/feature-block';
+import { getLayout } from '@components/layout/layout';
+import FlashSaleBlock from '@components/product/feeds/flash-sale-product-feed';
+import BestSellerProductFeed from '@components/product/feeds/best-seller-product-feed';
 import NewArrivalsProductFeed from '@components/product/feeds/new-arrivals-product-feed';
+import { useEffect } from 'react';
 import { ROUTES } from '@lib/routes';
+import { useUI } from '@contexts/ui.context';
 import {
-  masonryBanner,
-  promotionBanner,
-  modernDemoBanner as banner,
-  modernDemoProductBanner as productBanner,
+  standardDemoBanner as banner,
+  standardDemoHeroBanner as heroBanner,
+  standardDemoPromotionBanner as promotionalBanner,
 } from '@data/static/banners';
+import { collectionData } from '@data/static/collection';
 
-export { getStaticProps } from '@framework/homepage/modern';
+export { getStaticProps } from '@framework/homepage/standard';
 
 export default function Home() {
+  const { openModal, setModalView } = useUI();
+
+  useEffect(() => {
+    setModalView('NEWSLETTER_VIEW');
+    setTimeout(() => {
+      openModal();
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <BannerBlock data={masonryBanner} />
+      <HeroBlock data={heroBanner} />
       <Container>
-        <ProductsFlashSaleBlock />
-      </Container>
-      <BannerSliderBlock data={promotionBanner} />
-      <Container>
-        <CategoryBlock
-          sectionHeading="text-shop-by-category"
-          variant="rounded"
-        />
-        <ProductsFeatured sectionHeading="text-featured-products" />
+        <FlashSaleBlock />
+        <BannerCarouselBlock banners={promotionalBanner} />
+        <CategoryBlock sectionHeading="text-shop-by-category" />
+        <Divider />
+        <BestSellerProductFeed />
         <BannerCard
-          data={banner[0]}
-          href={`${ROUTES.COLLECTIONS}/${banner[0].slug}`}
+          data={banner}
+          href={`${ROUTES.COLLECTIONS}/${banner.slug}`}
           className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
-          classNameInner="aspect-[3.15/1]"
+          classNameInner="md:aspect-[6.7/1] aspect-[3.6/1]"
         />
-        <BrandGridBlock sectionHeading="text-top-brands" />
-        <BannerCard
-          data={banner[1]}
-          href={`${ROUTES.COLLECTIONS}/${banner[1].slug}`}
-          className="mb-12 lg:mb-14 xl:mb-16 pb-0.5 lg:pb-1 xl:pb-0"
-          classNameInner="aspect-[4.3/1]"
-        />
-        <BannerWithProducts
-          sectionHeading="text-on-selling-products"
-          categorySlug="/search"
-          data={productBanner}
-        />
-        <ExclusiveBlock />
         <NewArrivalsProductFeed />
-        <DownloadApps />
+        <Divider />
+        <BrandBlock sectionHeading="text-top-brands" />
+        <CollectionBlock data={collectionData} />
+        <FeatureBlock />
+        <DownloadApps className="bg-linen" />
         <Support />
-        <Instagram />
-        <Subscription className="px-5 py-12 bg-opacity-0 sm:px-16 xl:px-0 md:py-14 xl:py-16" />
+        <Subscription className="px-5 bg-linen sm:px-8 md:px-16 2xl:px-24" />
       </Container>
-      <Divider className="mb-0" />
     </>
   );
 }
