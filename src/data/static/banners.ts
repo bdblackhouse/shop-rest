c1b65bb2bc810f3ad2351a3859ef39a7974e2ff1
Minhazul-Islam18 <minhaz019166@gmail.com>
2025-01-29 12:11:36 +0600

    ..

--- src/data/static/banners.ts
+++ src/data/static/banners.ts
@@ -60,12 +60,12 @@ export const masonryBanner = [
     slug: 'exclusive-sunglasses',
     image: {
       mobile: {
-        url: '/assets/images/banner/masonry/banner-mobile-4.jpg',
+        url: 'http://ecommerce.globalazmain.com/storage/banner/banner-1.jpg',
         width: 232,
         height: 232,
       },
       desktop: {
-        url: '/assets/images/banner/masonry/banner-4.jpg',
+        url: 'http://ecommerce.globalazmain.com/storage/banner/banner-1.jpg',
         width: 425,
         height: 425,
       },
@@ -78,12 +78,12 @@ export const masonryBanner = [
     slug: 'product-coupons',
     image: {
       mobile: {
-        url: '/assets/images/banner/masonry/banner-mobile-5.jpg',
+        url: 'http://ecommerce.globalazmain.com/storage/banner/banner-2.jpg',
         width: 232,
         height: 232,
       },
       desktop: {
-        url: '/assets/images/banner/masonry/banner-5.jpg',
+        url: 'http://ecommerce.globalazmain.com/storage/banner/banner-2.jpg',
         width: 425,
         height: 425,
       },
@@ -117,12 +117,12 @@ export const gridBanner = [
     slug: 'mens-collection',
     image: {
       mobile: {
-        url: '/assets/images/banner/grid/banner-mobile-1.jpg',
+        url: 'http://ecommerce.globalazmain.com/storage/banner/banner-1.jpg',
         width: 450,
         height: 140,
       },
       desktop: {
-        url: '/assets/images/banner/grid/banner-1.jpg',
+        url: 'http://ecommerce.globalazmain.com/storage/banner/banner-1.jpg',
         width: 885,
         height: 430,
       },
